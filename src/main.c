c8acf9e443f53c90c0022a7a6fae39749b5b201e
Artur Shamsutdinov <a.shamsutdinov@icloud.com>
2021-04-21 12:45:16 +0500

    Create window

--- src/main.c
+++ src/main.c
@@ -1,7 +1,101 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
+#include "constants.h"
 
-int main() {
-    // TODO: Do magic!
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+int isGameRunning = FALSE;
+
+int initializeWindow()
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        fprintf(stderr, "Error initializing SDL.\n");
+        return FALSE;
+    }
+    window = SDL_CreateWindow(
+        "My Raycaster",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        SDL_WINDOW_BORDERLESS);
+
+    if (!window)
+    {
+        fprintf(stderr, "Error creating SDL window.\n");
+        return FALSE;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer)
+    {
+        fprintf(stderr, "Error creating SDL renderer.\n");
+        return FALSE;
+    }
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+    return TRUE;
+}
+
+void destroyWindow()
+{
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+void render()
+{
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // TODO:
+    // render objects at current frame
+    SDL_RenderPresent(renderer);
+}
+
+void processInput()
+{
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    switch (event.type)
+    {
+    case SDL_QUIT:
+    {
+        isGameRunning = FALSE;
+        break;
+    }
+    case SDL_KEYDOWN:
+    {
+        if (event.key.keysym.sym == SDLK_ESCAPE)
+        {
+            isGameRunning = FALSE;
+            break;
+        }
+    }
+
+    default:
+        break;
+    }
+}
+
+void setup()
+{
+    // TODO: setup
+}
+
+int main()
+{
+    isGameRunning = initializeWindow();
+    setup();
+
+    while (isGameRunning)
+    {
+        processInput();
+        // update();
+        render();
+    }
+
+    destroyWindow();
 
     return 0;
 }
